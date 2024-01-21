3603db20e3659987fb12f00738b273220fb4c591
aidenseo <sn14188@students.cs.ubc.ca>
2024-01-20 20:39:01 -0800

    working on posting class

--- src/main/java/com/yourcompany/app/model/Posting.java
+++ src/main/java/com/yourcompany/app/model/Posting.java
@@ -1,6 +1,6 @@
 package com.yourcompany.app.model;
 
-import java.util.List;
+import java.util.ArrayList;
 
 public class Posting {
     public enum Status {
@@ -19,30 +19,97 @@ public class Posting {
         UX_UI_DESIGNER
     }
 
+    private boolean active;
     private final String postingName;
     private Channel channel;
+    private int spot;
     private Status status;
-    private String description;
-    private List<Role> roles;
-    private List<User> users_in_team;
+    private String contents;
+    private ArrayList<Role> rolesNeeded;
+    private ArrayList<User> usersInTeam;
 
-    public Posting(String name) {
+    public Posting(String name, Channel channel, int spot) {
+        this.active = true;
         this.postingName = name;
+        this.channel = channel;
+        this.spot = spot;
+        this.status = Status.STILL_SEARCHING;
+        this.contents = "";
+        this.rolesNeeded = new ArrayList<Role>();
+        this.usersInTeam = new ArrayList<User>();
+    }
+
+    public boolean isActive() {
+        return this.active;
+    }
+    public String getName() {
+        return this.postingName;
+    }
+
+    public Channel getChannel() {
+        return this.channel;
+    }
+
+    public int getSpot() {
+        return this.spot;
+    }
+
+    public void decreaseRemainingSpot() {
+        this.spot--;
+    }
+
+    public Status getStatus() {
+        return this.status;
+    }
+
+    public void setStatusSearching() {
         this.status = Status.STILL_SEARCHING;
     }
 
-    public void setChannel() {}
-
-    public void setRoles() {}
-
-    public void setStatus() {}
-
-    public void writeDescription(String description) {
-        this.description = description;
+    public void setStatusInCommunication() {
+        this.status = Status.IN_COMMUNICATION;
     }
 
-    // To check the connection to its test module
-    public String getName() {
-        return this.postingName;
+    public void setStatusFound() {
+        this.status = Status.FOUND;
+    }
+
+    public void setPostingClosed() {
+        this.setStatusFound();
+        this.active = false;
+        // and some function to disappear posting
+    }
+
+    public String getContents() {
+        return this.contents;
+    }
+
+    public void writeDescription(String description) {
+        this.contents = description;
+    }
+
+    public void clearDescription() {
+        this.contents = "";
+    }
+
+    public ArrayList<Role> getRolesNeeded() {
+        return this.rolesNeeded;
+    }
+
+    public ArrayList<User> getUsersInTeam() {
+        return this.usersInTeam;
+    }
+
+    public void addRole(Role role) {
+        this.rolesNeeded.add(role);
+        // should make it up to 4?
+    }
+
+    public void addUser(User user) {
+        this.usersInTeam.add(user);
+        decreaseRemainingSpot();
+        if (this.spot == 0) {
+            setPostingClosed();
+        }
     }
 }
